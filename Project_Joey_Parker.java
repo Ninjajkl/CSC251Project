f0ea0eb7c118bdd95a6c31c195a4a91287f0eee2
Joey Parker <Ninjajkl@gmail.com>
2022-03-09 22:41:36 -0500

    Added code to display all policy information the number of smokers, and the number of non-smokers
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Joey_Parker.java
+++ Project_Joey_Parker.java
@@ -56,31 +56,51 @@ public class Project_Joey_Parker
             //Adds the newly created Policy to the policyList ArrayList
             policyList.add(newPolicy);
          }
-         //Demonstrating the Get Functions
          
-         /*
-         //Prints out Policy Number
-         System.out.println("\n\nPolicy Number: " + testPolicy.getPolicyNumber());
-         //Prints out Provider Name
-         System.out.println("Provider Name: " + testPolicy.getProviderName());
-         //Prints out PolicyHolder's First Name
-         System.out.println("PolicyHolder's First Name: " + testPolicy.getFirstName());
-         //Prints out PolicyHolder's Last Name
-         System.out.println("PolicyHolder's Last Name: " + testPolicy.getLastName());
-         //Prints out PolicyHolder's Age
-         System.out.println("PolicyHolder's Age: " + testPolicy.getAge());
-         //Prints out PolicyHolder's Smoking Status
-         System.out.println("nPolicyHolder's Smoking Status: " + testPolicy.getSmokingStatus());
-         //Prints out PolicyHolder's Height
-         System.out.println("PolicyHolder's Height: " + testPolicy.getHeight() + " inches");
-         //Prints out PolicyHolder's Weight
-         System.out.println("PolicyHolder's Weight: " + testPolicy.getWeight() + " pounds");
-         //Calculates and prints out Policyholder's BMI
-         System.out.printf("PolicyHolder's BMI: $%,.2f\n", testPolicy.BMICalculator());
-         //Calculates and prints out Policy Price
-         System.out.printf("Policy Price: $%,.2f\n", testPolicy.calculateInsurancePrice());
-         */
+         //Closes the file as it will no longer be read from
          inputFile.close();
+         
+         //Used to track the number of smokers and non-smokers
+         int numSmokers = 0;
+         int numNonSmokers = 0;
+         for(int i = 0; i < policyList.size(); i++)
+         {
+            //Prints all information of each policy
+            //Prints out Policy Number
+            System.out.println("Policy Number: " + policyList.get(i).getPolicyNumber());
+            //Prints out Provider Name
+            System.out.println("Provider Name: " + policyList.get(i).getProviderName());
+            //Prints out PolicyHolder's First Name
+            System.out.println("PolicyHolder's First Name: " + policyList.get(i).getFirstName());
+            //Prints out PolicyHolder's Last Name
+            System.out.println("PolicyHolder's Last Name: " + policyList.get(i).getLastName());
+            //Prints out PolicyHolder's Age
+            System.out.println("PolicyHolder's Age: " + policyList.get(i).getAge());
+            //Prints out PolicyHolder's Smoking Status
+            System.out.println("nPolicyHolder's Smoking Status (smoker/non-smoker): " + policyList.get(i).getSmokingStatus());
+            //Prints out PolicyHolder's Height
+            System.out.println("PolicyHolder's Height: " + policyList.get(i).getHeight() + " inches");
+            //Prints out PolicyHolder's Weight
+            System.out.println("PolicyHolder's Weight: " + policyList.get(i).getWeight() + " pounds");
+            //Calculates and prints out Policyholder's BMI
+            System.out.printf("PolicyHolder's BMI: %,.2f\n", policyList.get(i).BMICalculator());
+            //Calculates and prints out Policy Price
+            System.out.printf("Policy Price: $%,.2f\n\n", policyList.get(i).calculateInsurancePrice());
+            
+            //Used to find number of smokers/non-smokers
+            if(policyList.get(i).getSmokingStatus().equals("smoker"))
+            {
+               numSmokers++;
+            }
+            else
+            {
+               numNonSmokers++;
+            }
+         }
+         
+         //Prints out number of Smokers/Nonsmokers
+         System.out.println("The number of policies with a smoker is: " + numSmokers);
+         System.out.println("The number of policies with a non-smoker is: " + numNonSmokers);
       }
       catch(IOException ex)
       {
