b07d3565a5f22e0e25b57a80bb77c4a40989ac1f
Joey Parker <Ninjajkl@gmail.com>
2022-03-09 21:03:40 -0500

    Added documentation comments
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -38,12 +38,21 @@ public class Policy
          weight = w;
    }
    
-   //Calculates the BMI of the policyholder through their weight and height
+   /*
+      Calculates the BMI of the policyholder through their weight and height
+      
+      @return calculated BMI
+   */
    public double BMICalculator()
    {
       return (weight*703)/(height*height);
    }
    
+   /*
+      Calculates the price of the insurance depending on age, smokingStatus, and BMI
+      
+      @return calculated price of the insurance
+   */
    public double calculateInsurancePrice()
    {
       double price = 600;
@@ -64,68 +73,149 @@ public class Policy
    }
    
    //All the Setter methods for each field
+   
+   /*
+      Sets the policyNumber from the given pNumber
+      
+      @param pNumber - policy number to be changed to
+   */
    public void setPolicyNumber(int pNumber)
    {
       policyNumber = pNumber;
    }
+   /*
+      Sets the providerName from the given pName
+      
+      @param pName - provider name to be changed to
+   */
    public void setProviderName(String pName)
    {
       providerName = pName;
    }
+   /*
+      Sets the firstName from the given fName
+      
+      @param fName - first name to be changed to
+   */
    public void setFirstName(String fName)
    {
       firstName = fName;
    }
+   /*
+      Sets the lastName from the given lName
+      
+      @param lName - last name to be changed to
+   */
    public void setLastName(String lName)
    {
       lastName = lName;
    }
+   /*
+      Sets the age from the given a
+      
+      @param a - age to be changed to
+   */
    public void setAge(int a)
    {
       age = a;
    }
+   /*
+      Sets the smokingStatus from the given sStatus
+      
+      @param sStatus - smoking status to be changed to
+   */
    public void setSmokingStatus(String sStatus)
    {
       smokingStatus = sStatus;
    }
+   /*
+      Sets the height from the given h
+      
+      @param h - height to be changed to
+   */
    public void setHeight(double h)
    {
       height = h;
    }
+   /*
+      Sets the weight from the given w
+      
+      @param w - weight to be changed to
+   */
    public void setWeight(double w)
    {
       weight = w;
    }
    
    //All the Getter methods for each field
+   /*
+      Returns the policyNumber
+      
+      @return policy number
+   */
    public int getPolicyNumber()
    {
       return policyNumber;
    }
+   /*
+      Returns the providerName
+      
+      @return provider name
+   */
    public String getProviderName()
    {
       return providerName;
    }
+   /*
+      Returns the firstName
+      
+      @return first name
+   */
    public String getFirstName()
    {
       return firstName;
    }
+   /*
+      Returns the lastName
+      
+      @return last name
+   */
    public String getLastName()
    {
       return lastName;
    }
+   /*
+      Returns the age
+      
+      @return age
+   */
    public int getAge()
    {
       return age;
    }
+   /*
+      Returns the smokingStatus
+      
+      @return smoking status
+   */
    public String getSmokingStatus()
    {
       return smokingStatus;
    }
+   /*
+      Returns the height
+      
+      @return height
+   */
    public double getHeight()
    {
       return height;
    }
+   /*
+      Returns the weight
+      
+      @return weight
+   */
    public double getWeight()
    {
       return weight;
