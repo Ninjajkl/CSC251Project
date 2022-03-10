3814b9b839bdd2ba4ab7303ca705074696270e13
Joey Parker <Ninjajkl@gmail.com>
2022-03-09 22:30:10 -0500

    Modified to read input from a file
    Modified to read input from a file

--- Project_Joey_Parker.java
+++ Project_Joey_Parker.java
@@ -1,71 +1,90 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
 
 public class Project_Joey_Parker
 {
    public static void main(String[] args)
    {
-      //creating a Scanner object to get input
-      Scanner keyboard = new Scanner(System.in);
-      //creating a Policy Object
-      Policy testPolicy = new Policy();
-      
-      //Asks for Policy Number
-      System.out.print("Please enter the Policy Number: ");
-      testPolicy.setPolicyNumber(keyboard.nextInt());
-      //Needed to prevent skipping after the use of nextInt()
-      keyboard.nextLine();
-      
-      //Asks for Provider Name
-      System.out.print("Please enter the Provider Name: ");
-      testPolicy.setProviderName(keyboard.nextLine());
-      
-      //Asks for Policyholder's First Name
-      System.out.print("Please enter the Policyholder's First Name: ");
-      testPolicy.setFirstName(keyboard.nextLine());
-      
-      //Asks for Policyholder's Last Name
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      testPolicy.setLastName(keyboard.nextLine());
-      
-      //Asks for Policyholder's Age
-      System.out.print("Please enter the Policyholder's Age: ");
-      testPolicy.setAge(keyboard.nextInt());
-      //Needed to prevent skipping after the use of nextInt()
-      keyboard.nextLine();
-      
-      //Asks for Policyholder's Smoking Status
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      testPolicy.setSmokingStatus(keyboard.nextLine());
-      
-      //Asks for Policyholder's Height
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      testPolicy.setHeight(keyboard.nextDouble());
-      
-      //Asks for Policyholder's Weight
-      System.out.print("Please enter the Policyholder's Weight (in inches): ");
-      testPolicy.setWeight(keyboard.nextDouble());
-      
-      //Demonstrating the Get Functions
-      
-      //Prints out Policy Number
-      System.out.println("\n\nPolicy Number: " + testPolicy.getPolicyNumber());
-      //Prints out Provider Name
-      System.out.println("Provider Name: " + testPolicy.getProviderName());
-      //Prints out PolicyHolder's First Name
-      System.out.println("PolicyHolder's First Name: " + testPolicy.getFirstName());
-      //Prints out PolicyHolder's Last Name
-      System.out.println("PolicyHolder's Last Name: " + testPolicy.getLastName());
-      //Prints out PolicyHolder's Age
-      System.out.println("PolicyHolder's Age: " + testPolicy.getAge());
-      //Prints out PolicyHolder's Smoking Status
-      System.out.println("nPolicyHolder's Smoking Status: " + testPolicy.getSmokingStatus());
-      //Prints out PolicyHolder's Height
-      System.out.println("PolicyHolder's Height: " + testPolicy.getHeight() + " inches");
-      //Prints out PolicyHolder's Weight
-      System.out.println("PolicyHolder's Weight: " + testPolicy.getWeight() + " pounds");
-      //Calculates and prints out Policyholder's BMI
-      System.out.printf("PolicyHolder's BMI: $%,.2f\n", testPolicy.BMICalculator());
-      //Calculates and prints out Policy Price
-      System.out.printf("Policy Price: $%,.2f\n", testPolicy.calculateInsurancePrice());
+      try
+      {
+         //Finds the PolicyInformation File and opens it
+         File file = new File("PolicyInformation.txt");
+         //Creating a Scanner object to read the file
+         Scanner inputFile = new Scanner(file);
+         //Creating an ArrayList to add Policies to
+         ArrayList<Policy> policyList = new ArrayList<Policy>();
+         //As long as the file has more lines to read, this while loop repeats
+         while(inputFile.hasNext())
+         {
+            //creating a Policy Object
+            Policy newPolicy = new Policy();
+            
+            //Asks for Policy Number
+            newPolicy.setPolicyNumber(inputFile.nextInt());
+            
+            //Needed to prevent skipping after the use of nextInt()
+            inputFile.nextLine();
+            
+            //Asks for Provider Name
+            newPolicy.setProviderName(inputFile.nextLine());
+            
+            //Asks for Policyholder's First Name
+            newPolicy.setFirstName(inputFile.nextLine());
+            
+            //Asks for Policyholder's Last Name
+            newPolicy.setLastName(inputFile.nextLine());
+            
+            //Asks for Policyholder's Age
+            newPolicy.setAge(inputFile.nextInt());
+            
+            //Needed to prevent skipping after the use of nextInt()
+            inputFile.nextLine();
+            
+            //Asks for Policyholder's Smoking Status
+            newPolicy.setSmokingStatus(inputFile.nextLine());
+            
+            //Asks for Policyholder's Height
+            newPolicy.setHeight(inputFile.nextDouble());
+            
+            //Asks for Policyholder's Weight
+            newPolicy.setWeight(inputFile.nextDouble());
+            
+            //Needed to prevent skipping after the use of nextInt()
+            inputFile.nextLine();
+            
+            //Adds the newly created Policy to the policyList ArrayList
+            policyList.add(newPolicy);
+         }
+         //Demonstrating the Get Functions
+         
+         /*
+         //Prints out Policy Number
+         System.out.println("\n\nPolicy Number: " + testPolicy.getPolicyNumber());
+         //Prints out Provider Name
+         System.out.println("Provider Name: " + testPolicy.getProviderName());
+         //Prints out PolicyHolder's First Name
+         System.out.println("PolicyHolder's First Name: " + testPolicy.getFirstName());
+         //Prints out PolicyHolder's Last Name
+         System.out.println("PolicyHolder's Last Name: " + testPolicy.getLastName());
+         //Prints out PolicyHolder's Age
+         System.out.println("PolicyHolder's Age: " + testPolicy.getAge());
+         //Prints out PolicyHolder's Smoking Status
+         System.out.println("nPolicyHolder's Smoking Status: " + testPolicy.getSmokingStatus());
+         //Prints out PolicyHolder's Height
+         System.out.println("PolicyHolder's Height: " + testPolicy.getHeight() + " inches");
+         //Prints out PolicyHolder's Weight
+         System.out.println("PolicyHolder's Weight: " + testPolicy.getWeight() + " pounds");
+         //Calculates and prints out Policyholder's BMI
+         System.out.printf("PolicyHolder's BMI: $%,.2f\n", testPolicy.BMICalculator());
+         //Calculates and prints out Policy Price
+         System.out.printf("Policy Price: $%,.2f\n", testPolicy.calculateInsurancePrice());
+         */
+         inputFile.close();
+      }
+      catch(IOException ex)
+      {
+         System.out.println("Something went wrong reading the file: " + ex.getMessage());
+      }
    }
 }
