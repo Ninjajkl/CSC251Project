e42afcd2f5661e73d0d155be8c2140a6a01097d6
Joey Parker <Ninjajkl@gmail.com>
2022-03-31 12:06:41 -0400

    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -3,12 +3,6 @@ public class Policy
    //Initializing Fields
          private int policyNumber;
          private String providerName;
-         private String firstName;
-         private String lastName;
-         private int age;
-         private String smokingStatus;
-         private double height;
-         private double weight;
        
    //No-arg constructor  
    public Policy()
@@ -16,12 +10,6 @@ public class Policy
       //Set default values for fields
          policyNumber = 0;
          providerName = "";
-         firstName = "";
-         lastName = "";
-         age = 0;
-         smokingStatus = "";
-         height = 0;
-         weight = 0;
    }
    
    //Constructor that accepts arguments
@@ -30,46 +18,6 @@ public class Policy
       //Set field values to the given argument values
          policyNumber = pNumber;
          providerName = pName;
-         firstName = fName;
-         lastName = lName;
-         age = a;
-         smokingStatus = sStatus;
-         height = h;
-         weight = w;
-   }
-   
-   /**
-      Calculates the BMI of the policyholder through their weight and height
-      
-      @return calculated BMI
-   */
-   public double BMICalculator()
-   {
-      return (weight*703)/(height*height);
-   }
-   
-   /**
-      Calculates the price of the insurance depending on age, smokingStatus, and BMI
-      
-      @return calculated price of the insurance
-   */
-   public double calculateInsurancePrice()
-   {
-      double price = 600;
-      if(age > 50)
-      {
-         price += 50;
-      }
-      if(smokingStatus.equals("smoker"))
-      {
-         price += 100;
-      }
-      double BMI = BMICalculator();
-      if(BMI > 35)
-      {
-         price += (BMI - 35) * 20;
-      }
-      return price;
    }
    
    //All the Setter methods for each field
@@ -92,60 +40,6 @@ public class Policy
    {
       providerName = pName;
    }
-   /**
-      Sets the firstName from the given fName
-      
-      @param fName - first name to be changed to
-   */
-   public void setFirstName(String fName)
-   {
-      firstName = fName;
-   }
-   /**
-      Sets the lastName from the given lName
-      
-      @param lName - last name to be changed to
-   */
-   public void setLastName(String lName)
-   {
-      lastName = lName;
-   }
-   /**
-      Sets the age from the given a
-      
-      @param a - age to be changed to
-   */
-   public void setAge(int a)
-   {
-      age = a;
-   }
-   /**
-      Sets the smokingStatus from the given sStatus
-      
-      @param sStatus - smoking status to be changed to
-   */
-   public void setSmokingStatus(String sStatus)
-   {
-      smokingStatus = sStatus;
-   }
-   /**
-      Sets the height from the given h
-      
-      @param h - height to be changed to
-   */
-   public void setHeight(double h)
-   {
-      height = h;
-   }
-   /**
-      Sets the weight from the given w
-      
-      @param w - weight to be changed to
-   */
-   public void setWeight(double w)
-   {
-      weight = w;
-   }
    
    //All the Getter methods for each field
    /**
@@ -165,59 +59,5 @@ public class Policy
    public String getProviderName()
    {
       return providerName;
-   }
-   /**
-      Returns the firstName
-      
-      @return first name
-   */
-   public String getFirstName()
-   {
-      return firstName;
-   }
-   /**
-      Returns the lastName
-      
-      @return last name
-   */
-   public String getLastName()
-   {
-      return lastName;
-   }
-   /**
-      Returns the age
-      
-      @return age
-   */
-   public int getAge()
-   {
-      return age;
-   }
-   /**
-      Returns the smokingStatus
-      
-      @return smoking status
-   */
-   public String getSmokingStatus()
-   {
-      return smokingStatus;
-   }
-   /**
-      Returns the height
-      
-      @return height
-   */
-   public double getHeight()
-   {
-      return height;
-   }
-   /**
-      Returns the weight
-      
-      @return weight
-   */
-   public double getWeight()
-   {
-      return weight;
    }
 }
